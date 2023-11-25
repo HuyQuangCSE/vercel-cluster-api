1921a7d2037cac3fcaaccc7bde3bb6bd30693c85
Huy Duong <74403674+HuyQuangCSE@users.noreply.github.com>
2023-11-25 16:36:14 +0700

    add vercel api config

---
+++ main.py
@@ -0,0 +1,44 @@
+import json
+import uvicorn
+import pandas as pd
+
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+df = pd.read_csv("clustered_products.csv")
+
+app = FastAPI()
+
+origins = ["*"]
+methods = ["*"]
+headers = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=methods,
+    allow_headers=headers,
+)
+
+
+@app.get("/")
+async def root():
+    return {"detail": "API's root"}
+
+
+@app.get("/products/{product_id}")
+async def get_product_list(product_id: int):
+    try:
+        cluster = df.loc[df["ProductID"] == product_id, "Cluster"].iloc[0]
+    except KeyError as exc:
+        raise HTTPException(  # pylint: disable=W0707
+            status_code=404, detail=f"Product not found, {exc}"
+        )
+    return_df = df[df["Cluster"] == cluster].sample(n=20).to_json(orient="records")
+    response = json.loads(return_df)
+    return response
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000, host="0.0.0.0")
